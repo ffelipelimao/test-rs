5316a81bc882e4c2d22790d1ced4d6067a09f2c0
Luiz Felipe Limao <lflimao@hotmail.com>
2023-10-30 22:55:20 -0300

    test

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+fn fizz_buzz() {
+    for i in 0..100 {
+        match (i % 3, i % 5) {
+            (0, 0) => println!("FizzBuzz"),
+            (0, _) => println!("Fizz"),
+            (_, 0) => println!("Buzz"),
+            _ => println!("{}", i),
+        }
+    }
+}
+
+fn main() {
+    fizz_buzz()
+}
+
+
